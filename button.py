b7f47727f8a2a6124555266780f6d0daa4ea6802
Blake <b.lake@outlook.com>
2015-02-27 14:49:39 -0600

    breaking loop after 3 presses

--- button.py
+++ button.py
@@ -1,26 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
+button = 7
+light1 = 11
+light2 = 15
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN) 
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+GPIO.setup(button, GPIO.IN) 
+GPIO.setup(light1, GPIO.OUT)
+GPIO.setup(light2, GPIO.OUT)
 
 inc = 0
 state = 0
 
 while True:
-	if (GPIO.input(7) == True):
+	if (GPIO.input(button) == True):
 		inc = inc + 1
-		print inc
+
 		if (inc == 1):
-			GPIO.output(11, GPIO.HIGH)
+			GPIO.output(light1, GPIO.HIGH)
 		elif (inc == 2):
-			GPIO.output(15, GPIO.HIGH)
+			GPIO.output(light2, GPIO.HIGH)
 		elif (inc == 3):
-			GPIO.output(11, GPIO.LOW)
-			GPIO.output(15, GPIO.LOW)
-			inc = 0
+			GPIO.output(light1, GPIO.LOW)
+			GPIO.output(light2, GPIO.LOW)
+			break	
 		time.sleep(0.5)
 					
 GPIO.cleanup()
