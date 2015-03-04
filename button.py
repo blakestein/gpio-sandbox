b538a237e3dbf4f59db4bc57d387b46551140e07
Blake <b.lake@outlook.com>
2015-03-04 15:01:21 -0600

    Interrupts use way less CPU

--- button.py
+++ button.py
@@ -6,17 +6,16 @@ light1 = 11
 light2 = 15
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(button, GPIO.IN) 
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
 GPIO.setup(light1, GPIO.OUT)
 GPIO.setup(light2, GPIO.OUT)
 
-inc = 0
-state = 0
-
-while True:
-	if (GPIO.input(button) == True):
-		inc = inc + 1
-
+def light(state):
+	inc = state	
+	try:
+		GPIO.wait_for_edge(button, GPIO.FALLING)
+		inc += 1
+		print inc
 		if (inc == 1):
 			GPIO.output(light1, GPIO.HIGH)
 		elif (inc == 2):
@@ -24,7 +23,13 @@ while True:
 		elif (inc == 3):
 			GPIO.output(light1, GPIO.LOW)
 			GPIO.output(light2, GPIO.LOW)
-			break	
-		time.sleep(0.5)
+		time.sleep(0.2)
 					
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+	if (inc < 3):
+		light(inc)
+	else:
+		light(0)
+light(0)
 GPIO.cleanup()
